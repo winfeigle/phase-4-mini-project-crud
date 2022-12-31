bd569359ceaf36aa64608b5e41d25c73a00499e2
Win Feigle <win.feigle@gmail.com>
2022-12-31 13:00:55 -0700

    Add CRUD routes

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,45 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    # GET /spices
+    def index
+        spices = Spice.all 
+        render json: spices, status: :ok
+    end
+
+    # SHOW /spices/:id
+    def show
+        spice = Spice.find(params[:id])
+        render json: spice, status: :ok
+    end
+
+    # CREATE /spices
+    def create
+        spice = Spice.create(spices_params)
+        render json: spice, status: :created
+    end
+
+    # UPDATE /spices/:id
+    def update
+        spice = Spice.find(params[:id])
+        spice.update(spices_params)
+        render json: spice, status: :accepted
+    end
+
+    # DESTROY /spices/:id
+    def destroy
+        spice = Spice.find(params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spices_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def render_not_found_response
+        render json: { error: "Spice not found" }
+    end
+end
